5e8894ac549d0e394eb4cfbe1173f5f2546d7559
LillyBilly <lihaveland@uia.no>
2025-11-20 12:27:41 +0100

    Adda tekst konflikt 1 og 2

--- oppstartspakke.py
+++ oppstartspakke.py
@@ -18,9 +18,44 @@ print("\nKonflikten eskalerer, og det begynner å påvirke teamets moral og frem
      )
 print("\nSom prosjektleder må du ta en beslutning om hvordan du skal håndtere dette. "
      )
+print("=*60")
 print("\nHva gjør du?")
-#Konflikt 2
+print("=60*")
+print("\nA: Arranger et møte med Silje og Sivert for å diskutere konflikten, " \
+      "slik at begge parter får uttrykt bekymringene sine og kan finne en felles løsning. " \
+     )
+print("\nB: Prioriter fremdrift og ta en beslutning på egenhånd uten å involvere de. " \
+     )
+#koding til konflikt 1 sal være her
 
+print("=*60")
+print("=*60")
+#Konflikt 2
+print("\nSamtidig som konflikten mellom Silje og Sivert, oppstår det en annen konflikt i teamet ditt. " \
+      "Denne gangen er det mellom Hamdi (fra kulturavdelingen) og Jabir (brukerrepresentant). " 
+     )
+print("\nHamdi ønsker en kontrollert løsning gjennom kommunens eksisterende plattform, " \
+      "mens Jabir ønsker et mer åpent, dialogbasert system med rom for spontane innspill. "
+     )
+print("\nForeløpig er uenigheten lavmælt, men du merker at frustrasjonen vokser. "
+     )
+print("\nI tillegg til uenighetene så vet du at prosjektet nærmer seg en viktig milepæl; " \
+      "første prototype skal være klar om tre uker. " \
+      "Stemningen er spent, kommunikasjonen hakkete, og Erling vet at hans neste valg kan, " \
+      "avgjøre om teamet beveger seg videre mot 'norming', eller blir stående fast i 'stormen'. "
+      )
+print("=*60")
+print("\nHva gjør du?")
+print("*60")
+print("\nA: Arranger en workshop med både Hamdi og Jabir, " \
+      "slik at de kan diskutere sine perspektiver og finne en felles vei fremover. " \
+     )
+print("\nB: Velg å avvente situasjonen og håpe at de finner ut av det selv, uten din inngripen. " \
+     )
+#koding til konflikt 2 skal være her
+
+print("=*60")
+print("=*60")
 #Konflikt 3
 
 #Slutten
