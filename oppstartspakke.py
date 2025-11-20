85d1302ecaaea30c60a4e218230d6aa4364f5b29
LillyBilly <lihaveland@uia.no>
2025-11-20 12:03:32 +0100

    Adder tekst til konflikt 1

--- oppstartspakke.py
+++ oppstartspakke.py
@@ -4,7 +4,16 @@
 # Variabler/dictionary
 
 #Konflikt 1
-
+print("\nDet opstår en konflikt mellom Silje og Sivert i teamet ditt." \
+      "Silje (UX-designer) mener at brukeropplevelsen må prioriteres høyest, "
+      "mens Sivert (IT-utvikler) insisterer på at tekniske løsninger må komme først. "
+     )
+print("\nKonflikten eskalerer, og det begynner å påvirke teamets moral og fremdrift. " \
+      "Sakskonflikten har blitt til en personlig konflikt, og de begge drar støtte av andre teammedlemmer. "
+     )
+print("\nSom prosjektleder må du ta en beslutning om hvordan du skal håndtere dette. "
+     )
+print("\nHva gjør du?")
 #Konflikt 2
 
 #Konflikt 3
